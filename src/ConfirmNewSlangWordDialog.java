0af644edf94a984c48ff18de0446c2d5b4567109
Phuong Dao <phuong.dao@nabstudio.com>
2021-04-30 22:37:13 +0700

    add dialog confirm

---
+++ src/ConfirmNewSlangWordDialog.java
@@ -0,0 +1,38 @@
+package my;
+
+import java.awt.Dimension;
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.SwingConstants;
+import javax.swing.UIManager;
+
+public class ConfirmNewSlangWordDialog {
+    public static void main(String[] args) {
+        ImageIcon icon = new ImageIcon("E −\\new.PNG");
+        JPanel panel = new JPanel();
+        panel.setSize(new Dimension(250, 100));
+        panel.setLayout(null);
+        JLabel label1 = new JLabel("Slang word is duplicate");
+        label1.setVerticalAlignment(SwingConstants.BOTTOM);
+        label1.setBounds(20, 20, 200, 30);
+        label1.setHorizontalAlignment(SwingConstants.CENTER);
+        panel.add(label1);
+        JLabel label2 = new JLabel("Do you still want to save it?");
+        label2.setVerticalAlignment(SwingConstants.TOP);
+        label2.setHorizontalAlignment(SwingConstants.CENTER);
+        label2.setBounds(20, 80, 200, 20);
+        panel.add(label2);
+        UIManager.put("OptionPane.minimumSize", new Dimension(400, 200));
+        int res = JOptionPane.showConfirmDialog(null, panel, "File", JOptionPane.YES_NO_CANCEL_OPTION,
+                JOptionPane.PLAIN_MESSAGE, icon);
+        if (res == 0) {
+            System.out.println("Pressed Duplicate");
+        } else if (res == 1) {
+            System.out.println("Pressed Overwrite");
+        } else {
+            System.out.println("Pressed Cancel");
+        }
+    }
+}
