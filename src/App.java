0094384e2d81c598e0bbfad5304f6be632f765cf
Phuong Dao <phuong.dao@nabstudio.com>
2021-04-30 22:32:00 +0700

    add file lasted slang

--- src/App.java
+++ src/App.java
@@ -1,7 +1,13 @@
 import java.util.*;
+import java.io.*;
 
 public class App {
     public static void main(String[] args) throws Exception {
+
+        File f = new File("src/lasted-slang.txt");
+        if (f.exists() && !f.isDirectory()) {
+            // do something
+        }
 
         Map<String, String> values = Slang.TXTImport("src/slang.txt");
 
